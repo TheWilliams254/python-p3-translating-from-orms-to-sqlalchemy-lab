e0835b66aa62d9d92027e988d1a691466981f0fe
william <williamwambugu663@gmail.com>
2025-05-25 11:44:01 +0300

    CRUD

--- lib/dog.py
+++ lib/dog.py
@@ -1,22 +1,31 @@
 from models import Dog
 
-def create_table(base):
-    pass
+def create_table(base, engine):
+    """Create the database table for the Dog model."""
+    base.metadata.create_all(engine)
 
 def save(session, dog):
-    pass
+    """Save a Dog instance to the database."""
+    session.add(dog)
+    session.commit()
 
 def get_all(session):
-    pass
+    """Return a list of all Dog instances in the database."""
+    return session.query(Dog).all()
 
 def find_by_name(session, name):
-    pass
+    """Return a Dog instance corresponding to the record with the given name."""
+    return session.query(Dog).filter_by(name=name).first()
 
 def find_by_id(session, id):
-    pass
+    """Return a Dog instance corresponding to the record with the given id."""
+    return session.get(Dog, id)
 
 def find_by_name_and_breed(session, name, breed):
-    pass
+    """Return a Dog instance corresponding to the record with the given name and breed."""
+    return session.query(Dog).filter_by(name=name, breed=breed).first()
 
 def update_breed(session, dog, breed):
-    pass
+    """Update the breed of the given Dog instance in the database."""
+    dog.breed = breed
+    session.commit()
